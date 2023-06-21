1e9106297c3897fa84a31c63689dd58fb2acfaa4
Pablo Barbosa (Innov) <pablo.barbosa@innovsoftware.com.br>
2023-06-20 23:49:41 -0300

    Teste 1

--- WebAPIWithAuthenticationToken/Configuration/Settings.cs
+++ WebAPIWithAuthenticationToken/Configuration/Settings.cs
@@ -2,6 +2,6 @@
 {
     public static class Settings
     {
-        public static string ApplicationID = "5B71D5BF-FCFC-494D-A9F0-92A8733985C0";
+        public static string ApplicationID = "5B71D5BF-FCFC-494D-A9F0-92A8733985C0*";
     }
 }
